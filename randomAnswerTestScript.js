8c70825e0c4d8935e4f30ef5ff392b2ce13a8f23
axelmartini <erik.cronqvist@gmail.com>
2017-03-15 10:54:18 +0100

    Update randomAnswerTestScript.js

--- randomAnswerTestScript.js
+++ randomAnswerTestScript.js
@@ -1,58 +1,137 @@
+var req = new XMLHttpRequest();
+var questionsObject;
+var database = firebase.database();
+var firebaseRoot = firebase.database().ref();
+var datapath = firebase.database().ref("Questions");
+var findAQuestion = firebase.database().ref("Questions/1");
+var randomPicker;
+
+//console.log(findAQuestion.key);
+findAQuestion.once("value", getSnapshot);
+
+function getSnapshot(snapshot) {
+    console.log("snapshot");
+    console.log(snapshot.val());
+    console.log(snapshot.val().correct);
+
+}
+//console.log(findAQuestion.val());
+
+//console.log(firebaseRoot);
+//console.log(datapath);
+//console.log(findAQuestion);
+
 console.log('Script running');
 //Wait till all content is loaded, could be external fonts scripts from other servers etc....
-if (document.readyState != 'loading'){
-  onDocumentReady();
+if (document.readyState != 'loading') {
+    onDocumentReady();
 } else {
-  document.addEventListener('DOMContentLoaded', onDocumentReady);
+    document.addEventListener('DOMContentLoaded', onDocumentReady);
 }
 
 // Page is loaded! Now event can be wired-up
 function onDocumentReady() {
-  console.log('Document ready.');
-  displayQuestion();
+    console.log('Document ready.');
+    readQuestions();
+
+
 }
 
 
+
+function readQuestions() {
+    var questionsURL = "https://banalstravan-3b02a.firebaseio.com/.json";
+    req.open("GET", questionsURL, true);
+    req.addEventListener("load", questionsHandler);
+    req.send();
+}
+
+function questionsHandler() {
+    //console.log("req.response: " + req.response);
+    questionsObject = JSON.parse(req.response);
+    //console.log("Exercises Object: " + exercisesObject);
+
+    var findUsername = Object.entries(questionsObject.Questions);
+    console.log(findUsername);
+    console.log(findUsername.length);
+    req.removeEventListener("load", questionsHandler); // Stop listening
+    pickQuestion();
+    displayQuestion();
+}
+
+function pickQuestion() {
+    var questionsLength = questionsObject.Questions;
+    console.log(questionsLength.length);
+    randomPicker = Math.round(Math.random() * questionsLength.length);
+    console.log("Random Picker: " + randomPicker);
+    console.log(questionsObject.Questions[randomPicker].question);
+}
+
 function displayQuestion() {
-  var question = "Who wrote Anna Karenina?";
-  var correct = "Leo Tolstoy";
-  var wrong1 = "Fyodor Dostoyevsky";
-  var wrong2 = "Alexander Pushkin";
-  var wrong3 = "Anton Chekov";
 
-  var answerBoxOne = document.getElementById("answer1");
-  var answerBoxTwo = document.getElementById("answer2");
-  var answerBoxThree = document.getElementById("answer3");
-  var answerBoxFour = document.getElementById("answer4");
-  console.log(answerBoxOne);
+    var currentQuestion = questionsObject.Questions;
 
-  var positionGenerator = Math.ceil(Math.random() * 4);
-  console.log(positionGenerator);
+    var answerBoxOne = document.getElementById("answer1");
+    var answerBoxTwo = document.getElementById("answer2");
+    var answerBoxThree = document.getElementById("answer3");
+    var answerBoxFour = document.getElementById("answer4");
+    console.log(answerBoxOne);
 
-  document.getElementById("question").innerHTML = question;
-  if (positionGenerator == 1) {
-    document.getElementById("answer1").innerHTML = correct;
-    document.getElementById("answer2").innerHTML = wrong1;
-    document.getElementById("answer3").innerHTML = wrong2;
-    document.getElementById("answer4").innerHTML = wrong3;
+    var positionGenerator = Math.ceil(Math.random() * 4);
+    console.log(positionGenerator);
 
-  }
-  if (positionGenerator == 2) {
-    document.getElementById("answer2").innerHTML = correct;
-    document.getElementById("answer1").innerHTML = wrong1;
-    document.getElementById("answer3").innerHTML = wrong2;
-    document.getElementById("answer4").innerHTML = wrong3;
-  }
-  if (positionGenerator == 3) {
-    document.getElementById("answer3").innerHTML = correct;
-    document.getElementById("answer1").innerHTML = wrong1;
-    document.getElementById("answer2").innerHTML = wrong2;
-    document.getElementById("answer4").innerHTML = wrong3;
-  }
-  if (positionGenerator == 4) {
-    document.getElementById("answer4").innerHTML = correct;
-    document.getElementById("answer1").innerHTML = wrong1;
-    document.getElementById("answer2").innerHTML = wrong2;
-    document.getElementById("answer3").innerHTML = wrong3;
-  }
+    document.getElementById("question").innerHTML = currentQuestion[randomPicker].question;
+    if (positionGenerator == 1) {
+        answerBoxOne.innerHTML = currentQuestion[randomPicker].correct;
+        answerBoxTwo.innerHTML = currentQuestion[randomPicker].wrong1;
+        answerBoxThree.innerHTML = currentQuestion[randomPicker].wrong2;
+        answerBoxFour.innerHTML = currentQuestion[randomPicker].wrong3;
+    }
+    if (positionGenerator == 2) {
+        answerBoxTwo.innerHTML = currentQuestion[randomPicker].correct;
+        answerBoxOne.innerHTML = currentQuestion[randomPicker].wrong1;
+        answerBoxThree.innerHTML = currentQuestion[randomPicker].wrong2;
+        answerBoxFour.innerHTML = currentQuestion[randomPicker].wrong3;
+    }
+    if (positionGenerator == 3) {
+        answerBoxThree.innerHTML = currentQuestion[randomPicker].correct;
+        answerBoxOne.innerHTML = currentQuestion[randomPicker].wrong1;
+        answerBoxTwo.innerHTML = currentQuestion[randomPicker].wrong2;
+        answerBoxFour.innerHTML = currentQuestion[randomPicker].wrong3;
+    }
+    if (positionGenerator == 4) {
+        answerBoxFour.innerHTML = currentQuestion[randomPicker].correct;
+        answerBoxOne.innerHTML = currentQuestion[randomPicker].wrong1;
+        answerBoxTwo.innerHTML = currentQuestion[randomPicker].wrong2;
+        answerBoxThree.innerHTML = currentQuestion[randomPicker].wrong3;
+    }
+
+    answerBoxOne.addEventListener("click", function() {
+        if (positionGenerator == 1) {
+            answerBoxOne.classList.add("correctGreen");
+        } else {
+            answerBoxOne.classList.add("wrongRed");
+        }
+    });
+    answerBoxTwo.addEventListener("click", function() {
+        if (positionGenerator == 2) {
+            answerBoxTwo.classList.add("correctGreen");
+        } else {
+            answerBoxTwo.classList.add("wrongRed");
+        }
+    });
+    answerBoxThree.addEventListener("click", function() {
+        if (positionGenerator == 3) {
+            answerBoxThree.classList.add("correctGreen");
+        } else {
+            answerBoxThree.classList.add("wrongRed");
+        }
+    });
+    answerBoxFour.addEventListener("click", function() {
+        if (positionGenerator == 4) {
+            answerBoxFour.classList.add("correctGreen");
+        } else {
+            answerBoxFour.classList.add("wrongRed");
+        }
+    });
 }
